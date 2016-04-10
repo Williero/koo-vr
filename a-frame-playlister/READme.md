270b83bc08fc913374917f3bafaa3994aa2c828c
sebscholl <sebscholl@gmail.com>
2016-04-09 22:06:59 -0400

    made algorithim to evenly space album covers in circle around camera, added 'look at' feature so albums are always facing you. Switched scene

---
+++ a-frame-playlister/READme.md
@@ -0,0 +1,6 @@
+
+For an element around a centre at (x, z), distance r, the element's centre should be positioned at:
+
+( x + r cos(2 k Math.PI / n), z + r sin(2 k Math.PI / n) )
+
+where n is the number of elements, and k is the "number" of the element you're currently positioning (between 1 and n inclusive).
--- a-frame-playlister/playlister.js
+++ a-frame-playlister/playlister.js
@@ -1,12 +1,14 @@
 $(document).ready(function() {
-    var url = "https://api.spotify.com/v1/tracks/?ids=0eGsygTp906u18L0Oimnem,1lDWb6b6ieDQ2xT7ewTC3G,3hB5DgAiMAQ4DzYbsMq1IT,3pzjHKrQSvXGHQ98dx18HI,2iuZJX9X9P0GKaE93xcPjk,7oK9VyNzrYvRFo7nQEYkWN" ;
+    var url = "https://api.spotify.com/v1/tracks/?ids=0eGsygTp906u18L0Oimnem,1lDWb6b6ieDQ2xT7ewTC3G,3xEqegrcyQtfhWeEMnDyCG,3hB5DgAiMAQ4DzYbsMq1IT,3xEqegrcyQtfhWeEMnDyCG,3pzjHKrQSvXGHQ98dx18HI,2iuZJX9X9P0GKaE93xcPjk,7oK9VyNzrYvRFo7nQEYkWN,3pVbA69fLbVMwEv7ESCTm5" ;
     $.get(url, function(data) {
         var tracks = data.tracks ;
         for (var i in tracks ) {
             var track = tracks[i],
-                thumbnail = track.album.images[0].url ;
-            var album = '<a-entity class="album" geometry="primitive: box; width: .8; height: .8; depth: .1" material="src: url(' + thumbnail + ')" position="0 ' + i + ' -5" sound="src: ' + track.preview_url + '"></a-entity>'
-            $(album).insertAfter("#shelf")
+                thumbnail = track.album.images[0].url,
+                positionX = (0 + 3 * Math.cos(2 * i * Math.PI / tracks.length )),
+                positionZ = (0 + 3 * Math.sin(2 * i * Math.PI / tracks.length ));
+            var album = '<a-entity class="album" look-at="#camera" geometry="primitive: box; width: .8; height: .8; depth: .1" position="' + positionX + ' 1.25 ' + positionZ + '" material="src: url(' + thumbnail + ')" sound="src: ' + track.preview_url + '"></a-entity>'
+            $("#shelf").append(album)
         }
     }).success(function() {
 
@@ -28,16 +30,6 @@ $(document).ready(function() {
 
       }
 
-    });
-
-    document.querySelector('#hexagon').addEventListener('mouseenter', function () {
-        this.setAttribute('material', 'color', 'blue');
-        console.log("mouseenter " + this)
-    });
-
-    document.querySelector('#hexagon').addEventListener('mouseleave', function () {
-        this.setAttribute('material', 'color', 'green');
-        console.log("mouseenter " + this)
     });
 
 })
